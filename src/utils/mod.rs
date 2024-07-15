a962d39c3543f770b0d74d832f0b77abb9468d7c
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-07-16 00:05:35 +0200

    *

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,3 +1,23 @@
-mod location;
-pub mod syntax;
-pub use location::Location;
+#![allow(dead_code)]
+
+pub fn debug(raw: &[char], message: &str) -> String {
+  let mut line = 1;
+  let mut column = 0;
+  let mut i = 0;
+  while i < raw.len() {
+    if raw[i] == '\n' {
+      line += 1;
+      column = 0;
+    } else {
+      column += 1;
+    }
+    i += 1;
+  }
+  format!(
+    "{}:{}: {}\n{}",
+    line,
+    column,
+    message,
+    raw.iter().take(i).collect::<String>()
+  )
+}
