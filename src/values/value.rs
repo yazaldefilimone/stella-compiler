6713da944cecabea1348c2a5c1e6300d13941556
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-07-16 00:05:35 +0200

    *

---
+++ src/values/value.rs
@@ -0,0 +1,15 @@
+// Copyright 2024 Yazalde Filimone <yazalde.filimone@gmail.com>
+// Lua Virtual Machine Values
+#![allow(dead_code)]
+
+use crate::vm::ExeState;
+
+#[derive(Debug, Clone)]
+pub enum Value {
+  Nil,
+  Boolean(bool),
+  Number(f64),
+  String(String),
+  Function(fn(&mut ExeState) -> i32),
+  // Table(Table),
+}
