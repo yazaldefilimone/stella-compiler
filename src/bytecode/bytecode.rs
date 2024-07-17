d1e95158dfc7e9389c27437c1372eaba3eac3e7e
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-07-17 12:30:29 +0200

    *

--- src/bytecode/bytecode.rs
+++ src/bytecode/bytecode.rs
@@ -3,4 +3,9 @@ pub enum ByteCode {
   LoadConstant(u8, u8),
   GetGlobal(u8, u8),
   Call(u8, u8),
+  LoadBoolean(u8, bool),
+  LoadInteger(u8, i64),
+  LoadFloat(u8, f64),
+  LoadNil(u8),
+  Nop,
 }
