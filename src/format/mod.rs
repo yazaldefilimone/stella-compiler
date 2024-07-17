f1fb36c17ff727da4cb10d99a6a7ac88f889cad5
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-07-17 12:30:29 +0200

    *

--- src/format/mod.rs
+++ src/format/mod.rs
@@ -3,13 +3,14 @@ use std::fmt::Display;
 use crate::values::Value;
 
 impl Display for Value {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+  fn fmt(&self, format: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     match self {
-      Value::Nil => write!(f, "nil"),
-      Value::Boolean(b) => write!(f, "{}", b),
-      Value::Number(n) => write!(f, "{}", n),
-      Value::String(s) => write!(f, "{}", s),
-      Value::Function(_) => write!(f, "function"),
+      Value::Nil => write!(format, "nil"),
+      Value::Boolean(boolean) => write!(format, "{}", boolean),
+      Value::String(string) => write!(format, "{}", string),
+      Value::Function(_) => write!(format, "function"),
+      Value::Integer(integer) => write!(format, "{}", integer),
+      Value::Float(float) => write!(format, "{}", float),
     }
   }
 }
